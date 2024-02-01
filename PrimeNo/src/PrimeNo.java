6a78a4293151d24656a59384879ebd9bb4437f8e
kutub5 <kutuboffical@gmail.com>
2024-02-01 02:30:49 -0800

    US-kutub-PrimeNo

---
+++ PrimeNo/src/PrimeNo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class PrimeNo {
+		
+public static void main(String[] args) {
+	int xx;                                                          
+	int yy = 0;
+	System.out.println("Enter Any Number");                     
+	Scanner sc = new Scanner(System.in);
+	xx = sc.nextInt();
+	
+	for(int i=1;i<=xx;i++) {
+		
+		if(xx %i==0)
+		{
+        yy++;       
+	}	
+}
+       if(yy==2)
+    System.out.println("Number is a...PRIME");
+       else
+    System.out.println("Number is NOT a...PRIME");	
+   
+    }
+    
+
+	}
+
+
